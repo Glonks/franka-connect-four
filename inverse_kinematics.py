4e9ecc2a19964e48d2c321cb5928921f6d824e87
Glonks <kkhanna.rdl@gmail.com>
2026-04-02 14:41:40 -0400

    fox ik error calculation

--- inverse_kinematics.py
+++ inverse_kinematics.py
@@ -2,6 +2,10 @@ import numpy as np
 import mujoco as mj
 import copy
 
+from scipy.spatial.transform import Rotation as R
+
+from actions import CommonPoses
+
 
 class IKSolver:
     def __init__(
@@ -12,8 +16,9 @@ class IKSolver:
         position_tolerance=1e-3,
         rotation_tolerance=1e-3,
         step_size=0.5,
-        W = None,
-        C = None
+        W=None,
+        C=None,
+        alpha=0.1
     ):
         W = W if W is not None else np.eye(7)
         C = C if C is not None else np.eye(6)
@@ -26,47 +31,32 @@ class IKSolver:
         self.step_size = step_size
         self.W_inv = np.linalg.inv(W)
         self.C_inv = np.linalg.inv(C)
+        self.alpha = alpha
 
         self.joint_limits = self.model.jnt_range[:7].T
 
-    def solve(self, data_original, target_pose):
+    def solve(self, data_original, target_pose, bias=None):
+        target_position, target_orientation = target_pose
+        target_orientation = R.from_quat(target_orientation, scalar_first=True)
+
         data = copy.deepcopy(data_original)
         nv = self.model.nv
 
         for _ in range(self.max_iterations):
             mj.mj_forward(self.model, data)
 
-            target_position, target_orientation = target_pose
-
             current_position = data.xpos[self.body_id].copy()
             position_error = target_position - current_position
 
-            current_orientation = data.xquat[self.body_id]
-            orientation_error = np.zeros(3)
-            # if np.dot(target_orientation, current_orientation) < 0:
-            #     target_orientation = -target_orientation
-            mj.mju_subQuat(orientation_error, target_orientation, current_orientation)
+            current_orientation = R.from_quat(data.xquat[self.body_id].copy(), scalar_first=True)
+            orientation_error = (target_orientation * current_orientation.inv()).as_rotvec()
 
             position_error_norm = np.linalg.norm(position_error)
             orientation_error_norm = np.linalg.norm(orientation_error)
 
-            position_error_scaled = position_error
-            if position_error_norm > 0:
-                position_error_scaled = (position_error / position_error_norm) * min(position_error_norm, self.step_size)
-
-            orientation_error_scaled = orientation_error
-            if orientation_error_norm > 0:
-                axis = orientation_error / orientation_error_norm
-                orientation_error_scaled = axis * min(orientation_error_norm, self.step_size)
-
             error = np.concatenate([
                 position_error,
                 orientation_error
-            ])
-
-            error_scaled = np.concatenate([
-                position_error_scaled,
-                orientation_error_scaled
             ])
 
             converged = (
@@ -88,8 +78,13 @@ class IKSolver:
             J = np.vstack([J_position, J_orientation])[:, :7]
 
             J_prime = self.W_inv @ J.T @ np.linalg.inv(J @ self.W_inv @ J.T + self.C_inv)
+            dq = J_prime @ error
 
-            data.qpos[:7] += J_prime @ error_scaled
+            if bias is not None:
+                J_null = np.eye(7) - J_prime @ J
+                dq += self.alpha * (J_null @ (bias - data.qpos[:7]))
+
+            data.qpos[:7] += self.step_size * dq
             data.qpos[:7] = np.clip(
                 data.qpos[:7],
                 self.joint_limits[0],
